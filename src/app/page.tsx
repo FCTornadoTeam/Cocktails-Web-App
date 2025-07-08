7be079c9fa4b7c0bb0dd90281f852f306c4891d9
Semen Vodolazskyi <vodolazskijsemen@gmail.com>
2025-07-08 15:24:39 +0300

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <div className={styles.page}>
       <h1>Hello, this is a Cocktails Web App!</h1>
       <p>My name is David</p>
+      <p>
+        This is a simple web app that allows you to search for cocktails and
+        view their details.
+      </p>
     </div>
   );
 }
