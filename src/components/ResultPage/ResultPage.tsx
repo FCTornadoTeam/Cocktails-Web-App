e1785cf7674443837b9b43755a4810346f4dbd06
Semen Vodolazskyi <vodolazskijsemen@gmail.com>
2025-07-13 21:11:08 +0300

    implemented header and SearchBy section

---
+++ src/components/ResultPage/ResultPage.tsx
@@ -0,0 +1,9 @@
+import styles from "./ResultPage.module.scss";
+
+export const ResultPage = () => {
+  return (
+    <div className={styles.resultPage_container}>
+      <h2 className={styles.title}>Search results</h2>
+    </div>
+  );
+};
