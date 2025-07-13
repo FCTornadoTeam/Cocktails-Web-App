272a2706552b3918b487e35b246337c6844d531b
Semen Vodolazskyi <vodolazskijsemen@gmail.com>
2025-07-13 21:42:38 +0300

    added deploy x2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repo = 'Cocktails-Web-App'; // имя твоего репозитория
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
