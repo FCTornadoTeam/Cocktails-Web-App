6a938919abab954ffadc91c76cf6014d07638611
Semen Vodolazskyi <vodolazskijsemen@gmail.com>
2025-07-18 11:01:24 +0300

    card

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isGithubPages = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isGithubPages ? '/Cocktails-Web-App' : '',
-  assetPrefix: isGithubPages ? '/Cocktails-Web-App/' : '',
   images: {
     unoptimized: true,
   },
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,41 @@
 import Image from "next/image";
 import styles from "./Card.module.scss";
-import exampleCocktail from "../../public/image.png";
 
-export const Card = () => {
+type Card = {
+  title: string;
+  image: string;
+  category: string;
+  tags?: string | null;
+};
+
+export const Card: React.FC<Card> = ({ title, image, category, tags }) => {
   return (
     <div className={styles.card_container}>
       <div className={styles.card_header}>
-        <h3 className={styles.card_title}>Title</h3>
+        <h3 className={styles.card_title}>{title}</h3>
         <button className={styles.card_favorite_button}></button>
       </div>
-      <Image
-        src={exampleCocktail}
-        alt="Cocktail"
-        className={styles.card_image}
-        width={300}
-        height={200}
-      />
-      <p className={styles.card_description}>Category</p>
+      <div className={styles.card_image_wrapper}>
+        <Image
+          src={image}
+          alt={title}
+          fill
+          className={styles.card_image}
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <p className={styles.card_description}>{category}</p>
       <div className={styles.card_tags}>
-        <div className={styles.tag}>Tag</div>
-        <div className={styles.tag}>Tag</div>
-        <div className={styles.tag}>Tag</div>
-        <div className={styles.tag}>Tag</div>
+        {typeof tags === "string" && tags
+          ? tags.split(",").map((tag, index) => (
+              <div key={index} className={styles.tag}>
+                {tag.trim()}
+              </div>
+            ))
+          : null}
       </div>
       <a href="#" className={styles.card_details_link}>
-        <div className={styles.card_details_button}>Details</div>
+        <div className={tags ? styles.card_details_button : styles.card_details_button_down}>Details</div>
       </a>
     </div>
   );
