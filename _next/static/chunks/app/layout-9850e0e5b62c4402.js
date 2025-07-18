3cdb49ed615be4a6944729546e3131060451f2c7
Semen Vodolazskyi <vodolazskijsemen@gmail.com>
2025-07-18 11:25:54 +0300

    Updates

---
+++ _next/static/chunks/app/layout-9850e0e5b62c4402.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{9902:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,5356,23)),Promise.resolve().then(a.t.bind(a,2032,23))},2032:()=>{},5356:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_2faded"}}},e=>{var s=s=>e(e.s=s);e.O(0,[882,441,517,358],()=>s(9902)),_N_E=e.O()}]);
